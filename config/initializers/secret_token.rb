6f3be38a00ebfbc948a1686cafa11a9a41684f07
Ming Min Hui <ming@ming-VirtualBox.(none)>
2013-11-17 14:33:57 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mappr::Application.config.secret_key_base = 'bd0ca04038a6ae91bc0bfe328fc22a00842890ba343984a1bce4715198683956c08eb313ea8a6fde99255ca68fc6fa8e73fb9502337b8c93a25b11d57d652a78'
